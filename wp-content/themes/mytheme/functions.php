0ff7284d9a414de6b6ccd5297d8ee2391c2ea9d0
yannaing-lynn <rghtbossy@gmail.com>
2020-01-21 14:13:44 +0630

    event ok

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,26 +1,35 @@
 <?php
-function mytheme_files(){
-    wp_enqueue_script('main-university-js',get_theme_file_uri('/js/scripts-bundled.js'),NULL,microtime(),true);
-    wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font-awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles',get_stylesheet_uri(),NULL,microtime());
-}
-add_action('wp_enqueue_scripts','mytheme_files');
-function my_features(){
-    register_nav_menu('headerMenuLocation','Header Menu Location');
-    register_nav_menu('footerLocationOne','Footer Location One');
-    register_nav_menu('footerLocationTwo','Footer Location Two');
-    add_theme_support('title-tag');
-}
-add_action('after_setup_theme','my_features');
 
-function my_post_types(){
-    register_post_type('event', array(
-        'public'=>true,
-        'labels'=>array(
-            'name'=>'Events'
-        ),
-        'menu_icon'=>'dashicons-calendar'
-    ));
+function university_files() {
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
-add_action('init','my_post_types');
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
