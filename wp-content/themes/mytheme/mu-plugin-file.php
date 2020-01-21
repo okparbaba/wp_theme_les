458e71a33ddad897a596a413f17fd5477d861f6f
yannaing-lynn <rghtbossy@gmail.com>
2020-01-21 15:34:17 +0630

    program post type added

--- wp-content/themes/mytheme/mu-plugin-file.php
+++ wp-content/themes/mytheme/mu-plugin-file.php
@@ -18,5 +18,29 @@ function my_post_types(){
         ),
         'menu_icon'=>'dashicons-calendar'
     ));
+      //Programs Post Types
+      register_post_type('program', array(
+        'supports'=>array(
+            'title','editor'
+        ),
+        'rewrite'=>array(
+            'slug'=>'programs'
+        ),
+        'has_archive'=>true,
+        'public'=>true,
+        'labels'=>array(
+            'name'=>'Programs',
+            'add_new_item'=>'Add New Program',
+            'edit_item'=>'Edit Program',
+            'all_items'=>'All Programs',
+            'singlular_name'=>'Program'
+        ),
+        'menu_icon'=>'dashicons-awards'
+    ));
 }
 add_action('init','my_post_types');
+
+
+/*ဒီ ဖိုင် က theme folder ရဲ့ အပြင်မှာ mu-plugins ဆိုပြီးတော့ 
+ဖိုဒါတစ်ခုထက်ဆောက်ပြီးထည့်ရမဲ့ ဖိုင် ကြိုက်တဲ့နံမည်ပေးလို့ရတယ်။
+
